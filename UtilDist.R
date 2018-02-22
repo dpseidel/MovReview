d6679605ba46803e24a6c5f6c606714b32bcf209
Dana Seidel <dpseidel@berkeley.edu>
2018-02-22 10:22:44 -0800

    clean up Util file.

--- UtilDist.R
+++ UtilDist.R
@@ -39,88 +39,62 @@ points(AG_points, col = "green", cex= .3)
 
 
 
-# LoCoh - k 
-## Create convex hulls from the maximum number of nearest neighbors such that
-## the sum of their distances is less than or equal to a
+# LoCoh - a
+
 # using package t-locoh with s=0 is much faster than adehabitatHR `locoh.a` function
 AG_coords <- AG_wet %>% na.omit %>% st_coordinates 
 AG_time <- AG_wet %>% na.omit %>% pull(date)
 
 AG_lxy <- xyt.lxy(xy= AG_coords, dt= AG_time, id="AG256", proj4string=CRS("+proj=utm +south +zone=33 +ellps=WGS84"))
-AG_lxy <- lxy.nn.add(AG_lxy, s=0, k = 45)
-
 summary(AG_lxy)
-# testing k's 
-# creating different hullsets with ascending *k* values 9-45 by 3. 
-# we are still setting s=0 because we are not considering the temporal component.
-AG_lhs <- lxy.lhs(AG_lxy, k=3*3:15, s=0)
-plot(AG_lhs, hulls=TRUE, figs.per.page=6) # look for no swiss cheese?
 
-# or create isos look for asymptoting area, make sure no big jumps.
-AG_isos <- lhs.iso.add(AG_lhs)
-lhs.plot.isoarea(AG_isos)
-# look for k's that don't have high edge to area ration. 
-lhs.plot.isoear(AG_isos)
-
-# Ok, let's pick 30 to be conservative 
-AG_lhs_k30 <- lhs.select(AG_lhs, k=30)
-AG_iso_k30 <- lhs.iso.add(AG_lhs_k30)
-
-# pick out the 95% polygon.
-AG_lhs_k30_95poly <- AG_iso_k30$AG256.pts10791.k30.s0.kmin0$isos$`iso.srt-area.iso-q.h10791.i5`$polys[5,]
-plot(AG_lhs_k30_95poly)
-
-# compared to Locoh.a, 
 AG_lxy_a <- lxy.nn.add(AG_lxy, s=0, a = 80000)
 AG_lhs_a <- lxy.lhs(AG_lxy_a, a=seq(30000,80000,5000), s=0)
 AG_isos_a <- lhs.iso.add(AG_lhs_a)
 lhs.plot.isoarea(AG_isos_a)
 lhs.plot.isoear(AG_isos_a)
 
-AG_lhs_a36000 <- lhs.select(AG_lhs_a, a=75000)
-AG_iso_a36000 <- lhs.iso.add(AG_lhs_a36000)
-AG_lhs_a36000_95poly <- AG_iso_a36000[[1]]$isos[[1]]$polys[5]
-plot(AG_lhs_a36000_95poly)
+AG_lhs_a75000 <- lhs.select(AG_lhs_a, a=75000)
+AG_iso_a75000 <- lhs.iso.add(AG_lhs_a75000)
+AG_lhs_a75000_95poly <- AG_iso_a75000[[1]]$isos[[1]]$polys[5]
+plot(AG_lhs_a75000_95poly)
 
-# For figure
-
+# all together plot
 plot(AG_kud_95, col="orange")
 #points(AG_points, col = "grey", cex= .05)
-
 plot(AG_mcp_100, add=TRUE, col = )
 #points(AG_points, col = "grey", cex= .05)
-
-plot(AG_lhs_a36000_95poly, add = TRUE, col = "blue")
+plot(AG_lhs_a75000_95poly, add = TRUE, col = "blue")
 points(AG_points, col = "grey", cex= .05)
 
-# ggplots
+# individual plots
 hr <- st_as_sf(AG_mcp_100) %>% st_set_crs(32733)
 kud <- st_as_sf(AG_kud_95) %>% st_set_crs(32733)
-locoh <- st_as_sf(AG_lhs_a36000_95poly)
+locoh <- st_as_sf(AG_lhs_a75000_95poly)
 
-hr_plot <- ggplot(hr) + 
-  geom_sf(fill = "#9ecae1") + theme_light() + 
-  theme(axis.text = element_text(size = 20),
-        plot.title = element_text(size = 28, hjust = 0.5)) +
-  ggtitle("100% Minimum Convex Polygon") +
-  geom_sf(data=AG_wet, size = .1) 
-ggsave("MCP.eps", plot = hr_plot, width = 10, h = 8, units = "in")
-
-kud_plot <- ggplot() + 
-  geom_sf(data = kud, fill="#9ecae1") + 
-  my_theme +
-  ggtitle("95% Kernel Utilization Distribution") +
-  geom_sf(data=AG_wet, size = .1)
-ggsave("KUD.eps", plot = kud_plot, width = 10, h = 8, units = "in")
-
-locoh_plot <- ggplot() + 
-  geom_sf(data = locoh, fill = "#9ecae1") + 
-  theme_light() + 
-  theme(axis.text = element_text(size = 20),
-        plot.title = element_text(size = 28, hjust = 0.5)) +
-  ggtitle("95% fixed a-LoCoH, a=36000m") +
-  geom_sf(data=AG_wet, size = .1)
-ggsave("Locoh.eps", plot = locoh_plot, width = 10, h = 8, units = "in")
+# hr_plot <- ggplot(hr) + 
+#   geom_sf(fill = "#9ecae1") + theme_light() + 
+#   theme(axis.text = element_text(size = 20),
+#         plot.title = element_text(size = 28, hjust = 0.5)) +
+#   ggtitle("100% Minimum Convex Polygon") +
+#   geom_sf(data=AG_wet, size = .1) 
+# ggsave("MCP.eps", plot = hr_plot, width = 10, h = 8, units = "in")
+# 
+# kud_plot <- ggplot() + 
+#   geom_sf(data = kud, fill="#9ecae1") + 
+#   my_theme +
+#   ggtitle("95% Kernel Utilization Distribution") +
+#   geom_sf(data=AG_wet, size = .1)
+# ggsave("KUD.eps", plot = kud_plot, width = 10, h = 8, units = "in")
+# 
+# locoh_plot <- ggplot() + 
+#   geom_sf(data = locoh, fill = "#9ecae1") + 
+#   theme_light() + 
+#   theme(axis.text = element_text(size = 20),
+#         plot.title = element_text(size = 28, hjust = 0.5)) +
+#   ggtitle("95% fixed a-LoCoH, a=75000m") +
+#   geom_sf(data=AG_wet, size = .1)
+# ggsave("Locoh.eps", plot = locoh_plot, width = 10, h = 8, units = "in")
 
 ### gridless
 png(file = "MCP.png")
@@ -137,4 +111,30 @@ png(file = "LoCoh.png")
 plot(st_geometry(locoh), col="#9ecae1")
 points(st_coordinates(AG_wet), cex=.1)
 dev.off()
+
+
+# LoCoh - k  for comparison
+# testing k's 
+# creating different hullsets with ascending *k* values 9-45 by 3. 
+# # we are still setting s=0 because we are not considering the temporal component.
+# AG_lhs <- lxy.lhs(AG_lxy, k=3*3:15, s=0)
+# plot(AG_lhs, hulls=TRUE, figs.per.page=6) # look for no swiss cheese?
+# 
+# # or create isos look for asymptoting area, make sure no big jumps.
+# AG_isos <- lhs.iso.add(AG_lhs)
+# lhs.plot.isoarea(AG_isos)
+# # look for k's that don't have high edge to area ration. 
+# lhs.plot.isoear(AG_isos)
+# 
+# # Ok, let's pick 30 to be conservative 
+# AG_lhs_k30 <- lhs.select(AG_lhs, k=30)
+# AG_iso_k30 <- lhs.iso.add(AG_lhs_k30)
+# 
+# # pick out the 95% polygon.
+# AG_lhs_k30_95poly <- AG_iso_k30$AG256.pts10791.k30.s0.kmin0$isos$`iso.srt-area.iso-q.h10791.i5`$polys[5,]
+# plot(AG_lhs_k30_95poly)
+#
+# AG_lxy <- xyt.lxy(xy= AG_coords, dt= AG_time, id="AG256", proj4string=CRS("+proj=utm +south +zone=33 +ellps=WGS84"))
+# AG_lxy <- lxy.nn.add(AG_lxy, s=0, k = 45)
+
 
